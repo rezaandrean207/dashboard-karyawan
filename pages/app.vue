5af306ffc5017ad38f5a150249408bc73de6e1aa
Reza Andrean <sitiaisyah19277@gmail.com>
2026-04-13 15:50:59 +0700

    foto/logo di url

---
+++ pages/app.vue
@@ -0,0 +1,17 @@
+<script setup>
+useHead({
+  title: "Dashboard Karyawan",
+  meta: [
+    { property: "og:title", content: "Dashboard Karyawan" },
+    {
+      property: "og:description",
+      content: "Kelola dan pantau kinerja karyawan",
+    },
+    { property: "og:image", content: "https://clickup.devlmu.com/img/logo dna.png" },
+    { property: "og:url", content: "https://clickup.devlmu.com" },
+    { property: "og:type", content: "website" },
+  ],
+});
+</script>
+
+
